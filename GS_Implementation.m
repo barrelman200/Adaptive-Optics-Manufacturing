cabf6396fc4834f34e2c5ea904bf6a1f3f03b8d2
barrelman200 <aramisfereydoun@hotmail.com>
2025-04-19 14:41:18 +0100

    Copyright disclaimer

--- GS_Implementation.m
+++ GS_Implementation.m
@@ -1,3 +1,17 @@
+% © 2025 Aramis Marti Shahandeh (author)
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License version 3.0
+% as published by the Free Software Foundation.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program in the repo. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+ 
 %%  Parameters 
 lambda       = 800e-9;     % wavelength: 800 nm
 f_obj        = 8.0e-5;     % effective focal length of the objective
